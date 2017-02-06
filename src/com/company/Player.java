5674fbd57d6c05c0edfb7087cce65f708cb21300
sifer <max.ekstrom@mensa.se>
2017-02-06 15:45:12 +0100

    klasdklaskldas

--- src/com/company/Player.java
+++ src/com/company/Player.java
@@ -4,11 +4,31 @@ package com.company;
  * Created by Administrator on 2017-01-27.
  */
 public class Player {
-    public int x;
-    public int y;
-
+    
+	ölamlfäafmamsäfömfasmä
     public Player(int x, int y) {
         this.x = x;
         this.y = y;
+    }
+}
+public class Player {
+    public int x;
+    public int y;
+	klasdkpefnpqnoe
+    public Player(ix = x;
+        this.y = y;
+    }
+}
+public class Player {
+    public int x;
+    piw
+    public Player(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+public class Player {
+    public int x;
+    public in
     }
 }
