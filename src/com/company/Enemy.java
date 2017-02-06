d8a7510569c141db8aa198df7594bc219e69c03a
sifer <max.ekstrom@mensa.se>
2017-02-06 15:11:23 +0100

    Hej, jag ändrade massor så du får lite att göra under helgen.

--- src/com/company/Enemy.java
+++ src/com/company/Enemy.java
@@ -4,11 +4,11 @@ package com.company;
  * Created by Administrator on 2017-01-27.
  */
 public abstract class Enemy {
+	public float speed;
     public float x;
-    public float y;
-    public float speed;
+    // hej här kommer jag
+	public int derp;
     public char displaychar;
-
     public Enemy() {
     }
 }
