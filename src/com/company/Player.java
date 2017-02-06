7e10bec935153c4b8338717e80ab91ce4235053c
sifer <max.ekstrom@mensa.se>
2017-02-06 15:54:00 +0100

    Revert "klasdklaskldas"
    This reverts commit 5674fbd57d6c05c0edfb7087cce65f708cb21300.

--- src/com/company/Player.java
+++ src/com/company/Player.java
@@ -4,31 +4,11 @@ package com.company;
  * Created by Administrator on 2017-01-27.
  */
 public class Player {
-    
-	ölamlfäafmamsäfömfasmä
-    public Player(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
-}
-public class Player {
     public int x;
     public int y;
-	klasdkpefnpqnoe
-    public Player(ix = x;
-        this.y = y;
-    }
-}
-public class Player {
-    public int x;
-    piw
+
     public Player(int x, int y) {
         this.x = x;
         this.y = y;
-    }
-}
-public class Player {
-    public int x;
-    public in
     }
 }
