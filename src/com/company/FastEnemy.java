e88d795582ece752261834f7ea6ea0a88f2922e0
sifer <max.ekstrom@mensa.se>
2017-02-06 15:28:10 +0100

    laskldklasdkldkl

--- src/com/company/FastEnemy.java
+++ src/com/company/FastEnemy.java
@@ -4,12 +4,6 @@ package com.company;
  * Created by Administrator on 2017-02-02.
  */
 public class FastEnemy extends Enemy{
-
-
-    public FastEnemy(float x, float y) {
-        super.x = x;
-        super.y = y;
-        displaychar = 'F';
-        speed=0.7f;
+aökldflasklfafgilsopgasgä
     }
 }
