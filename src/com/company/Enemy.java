d4b0f1100b08015ff6392b6a24ede524b5fea1e1
sifer <max.ekstrom@mensa.se>
2017-02-06 15:52:41 +0100

    Revert "adklasdkl"
    This reverts commit e197f01747a84fe1c017481d9249e153cf0b147e.

--- src/com/company/Enemy.java
+++ src/com/company/Enemy.java
@@ -3,4 +3,12 @@ package com.company;
 /**
  * Created by Administrator on 2017-01-27.
  */
-//klaskldaklsdklwpihpriqpwhr
+public abstract class Enemy {
+	public float speed;
+    public float x;
+    // hej här kommer jag
+	public int derp;
+    public char displaychar;
+    public Enemy() {
+    }
+}
