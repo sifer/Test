49bb873a5c7d59a4cac8dbf23a25e0bb45a79e8f
sifer <max.ekstrom@mensa.se>
2017-02-06 15:29:44 +0100

    Nu jävlar

--- src/com/company/FastEnemy.java
+++ src/com/company/FastEnemy.java
@@ -8,8 +8,5 @@ public class FastEnemy extends Enemy{
 
     public FastEnemy(float x, float y) {
         super.x = x;
-        super.y = y;
-        displaychar = 'F';
-        speed=0.7f;
-    }
+       asälfälasäög'öasgl
 }
